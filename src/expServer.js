504d81946822b8febde5c123d80a709515d8103d
sanctus-fortis <addisonjrogers@tutanota.com>
2025-03-04 19:08:02 -0700

    Increased sarcasm factor

---
+++ src/expServer.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { data } = require('react-router-dom');
+
+const expServer = express();
+expServer.use(express.json());
+expServer.use(cors());
+
+const databaseConnection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+  });
+
+  //ROUTES YOU BIMBO
+  //Specifically register first. Check if the user is already in the DB. If so, tell them they're dumb or something.
+  expServer.post('/api/register', async (req, res) => {
+    const { username, password } = req.body;
+    databaseConnection.query('SELECT * FROM users WHERE username = ?', [username], async (err, result) => {
+      if (err) return res.status(500).json({ message: 'Nah mate couldn\'t find the database. Sanctus probably ran out of cash and had to take it down.' });
+      if (result.length > 0) return res.status(400).json({ message: 'Yeah, that guy\'s already in there mate. Probably try another username I guess.' });
+      const hashedPassword = await bcrypt.hash(password, 10);
+      databaseConnection.query('INSERT INTO users (username, password) VALUES (?, ?)', 
+        [username, hashedPassword], 
+        (err, result) => {
+          if (err) return res.status(500).json({ message: 'Nah mate couldn\'t find the database. Sanctus probably ran out of cash and had to take it down.' });
+          res.json({ message: 'You\'re on the list now mate. Watch what you do from here out.' });
+        }
+      );
+    });
+  });
+
+  const PORT = 5000;
+app.listen(PORT, () => console.log(`boop boop beep beep *digital signal over telephone cable noise* on ${PORT}`));
