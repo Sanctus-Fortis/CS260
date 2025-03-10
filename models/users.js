f418ed91f6b22032ad143b5278e85baa0897d202
sanctus-fortis <addisonjrogers@tutanota.com>
2025-03-10 12:42:37 -0600

    Remove old mongodb stuff

--- models/users.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-const UserSchema = new mysql.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
-
-module.exports = mongoose.model('User', UserSchema);
