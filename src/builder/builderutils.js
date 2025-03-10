a76b9dc68cb832b3daff9c2ebfa0fec76a3483c1
sanctus-fortis <addisonjrogers@tutanota.com>
2025-03-10 14:51:49 -0600

    pseudocoding logic

--- src/builder/builderutils.js
+++ src/builder/builderutils.js
@@ -24,6 +24,29 @@ export const fetchData = async () => {
     }
   };
 
+// Runs each time the sheet is changed to recalculate the adventurer build.
+// Add a calculate button? More use friendly?
+// Logic:
+// Grab attributes from each field
+// Grab race from field
+
+// Apply race attributes to total attributes
+  // strength += race.strengthmod
+  // dexterity += race.dexteritymod
+  // constitution += race.constitutionmod
+  // intelligence += race.intelligencemod
+  // wisdom += race.wisdommod
+  // charisma += race.charismamod
+
+// Get class from field
+// Add each proficiency to the skills list
+// Get weapon from field
+// Add weapon damage to total damage
+// Modify total damage based on proficiencies and attributes
+// If the weapons is a shield add armor value to total armor
+// Get armor from field
+// Add armor value to total armor
+
 export const calculateStats = (adventurer) => {
     const totalAttributes = Object.values(adventurer.attributes).reduce((acc, value) => acc + value, 0);
     return {
